ec0a5c3d5a74e18ab77e3bdd76f9b2cb34a477d0
Vitaly Ostrosablin <tmp6154@yandex.ru>
2024-06-08 18:24:17 +0300

    Update copyright dates

--- error.py
+++ error.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # YatzyBot - A Telegram bot for playing Yatzy/Yahtzee
-# Copyright (C) 2019  Vitaly Ostrosablin
+# Copyright (C) 2019-2024  Vitaly Ostrosablin
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
